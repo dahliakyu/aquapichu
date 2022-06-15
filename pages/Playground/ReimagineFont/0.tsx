044cecca9b025ee96a060ad2a0a1721dd7aca88f
dahliakyu <lamiyavi@hotmail.com>
2022-06-15 17:47:10 +0200

    on main

---
+++ pages/Playground/ReimagineFont/0.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Link from "next/link";
+
+const Blog = () => {
+  return (
+    <section className="bg-white dark:bg-stone-800">
+        <div className="max-w-4xl mx-auto h-48 bg-white dark:bg-stone-800">
+            <h1 className="text-4xl dark:text-stone-200 md:text-5xl font-bold pt-10 pb-5 text-left">
+                Reimagine font
+            </h1>
+            <hr></hr>
+            <div className = "text-xl dark:text-stone-400 md:text-2xl pt-5 text-left">
+                Working on it since mid-June 2022
+            </div> 
+        </div>
+            <div className="bg-[#F1F1F1] dark:bg-stone-900 grid grid-cols-1 md:grid-cols-1 leading-relaxed gap-8 py-20 pb-40 text-lg">
+                <div className="max-w-4xl mx-auto">
+                    I had the idea of this project inspired by a font designer friend (<a className = "text-yellow-900 hover:text-yellow-700 dark:text-orange-500" href={'https://www.instagram.com/jacob.j.wise/'}>Jacob</a>, a very talented elf) of mine, attended workshops for Hangul and Arabic font design and created beautiful typeface despite not being an expert in the languages himself. Then I thought maybe if I can build a web page that allows me to construct and manipulate Hangul characters, I can finally (perhaps) start to learn Korean properly.
+                </div>
+                <div className="max-w-4xl mx-auto">
+                    As it is an ongoing project, I will be updating the progress every now and then, keep on pressing the next button for the latest update!
+                </div>
+            </div>
+            <div className="max-w-4xl pt-5 mx-auto text-center md:text-right space-x-10 text-yellow-900 hover:text-yellow-700 dark:text-orange-500  dark:hover:text-orange-700">
+                <Link href={'1'}> Next &rarr; </Link>
+            </div>
+        </section>
+  )
+}
+
+export default Blog
