41d99c574bc5be8a674d4668012873c97f9d3015
dahliakyu <lamiyavi@hotmail.com>
2022-11-03 11:59:46 +0100

    yes

--- src/pages/Logs/Animate/ReactThreeFiber/OrbitingBirds/OrbitingBirds.js
+++ src/pages/Logs/Animate/ReactThreeFiber/OrbitingBirds/OrbitingBirds.js
@@ -1,13 +1,19 @@
 import RenderBirds from './RenderBirds'
-
+import DiscreteSlider from './tools/DiscreetSlider'
+import ChangeHandler from './tools/ChangeHandler'
+import ButtonEventHandler from './tools/ButtonEventHandler'
 const Blog = () => {
-  const BirdNumber = 200
+  const BirdNumber = 20
   return (
-    <div>
+    <section className="bg-white dark:bg-stone-800">
+      <div className="max-w-2xl mx-auto h-48 bg-white dark:bg-stone-800">
       <RenderBirds
         number = {BirdNumber}
-       />
-    </div>
+      /> 
+      <DiscreteSlider 
+      />
+      </div>
+    </section>
   )
 }
 
